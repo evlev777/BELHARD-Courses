8ba6db592b99e780a27ca2677cfbc033c1ab2e84
evlev777 <levkovich.evgeniy.98@gmail.com>
2023-06-29 14:48:12 +0300

    add try...except in task

--- Homework_3/lesson5_2.py
+++ Homework_3/lesson5_2.py
@@ -1,18 +1,20 @@
+try:
+    while True:
+        num1 = int(input('Первое число: '))
+        operation = input('Операция: ').replace(' ', '')
+        num2 = int(input('Первое число: '))
 
-while True:
-    num1 = int(input('Первое число: '))
-    operation = input('Операция: ').replace(' ', '')
-    num2 = int(input('Первое число: '))
+        if operation == '+':
+            print(f'{num1} {operation} {num2}', num1 + num2, sep=' = ')
+        elif operation == '-':
+            print(f'{num1} {operation} {num2}', num1 - num2, sep=' = ')
+        elif operation == '*':
+            print(f'{num1} {operation} {num2}', num1 * num2, sep=' = ')
+        elif operation == '/':
+            print(f'{num1} {operation} {num2}', num1 / num2, sep=' = ')
 
-    if operation == '+':
-        print(f'{num1} {operation} {num2}', num1 + num2, sep=' = ')
-    elif operation == '-':
-        print(f'{num1} {operation} {num2}', num1 - num2, sep=' = ')
-    elif operation == '*':
-        print(f'{num1} {operation} {num2}', num1 * num2, sep=' = ')
-    elif operation == '/':
-        print(f'{num1} {operation} {num2}', num1 / num2, sep=' = ')
+        if input('Введите n для выхода: ').lower() == 'n':
+            break
 
-    if input('Введите n для выхода: ').lower() == 'n':
-        break
-
+except ZeroDivisionError:
+    print('Ошибка: деление на 0')
