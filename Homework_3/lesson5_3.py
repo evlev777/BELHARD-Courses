9613ef3b3012ab6a6bd76a8fdafa31add55225b8
evlev777 <levkovich.evgeniy.98@gmail.com>
2023-06-29 15:06:27 +0300

    complete 3 task

---
+++ Homework_3/lesson5_3.py
@@ -0,0 +1,9 @@
+num = int(input('Введите конченое число: '))
+sub_str = ''
+
+for i in range(2, num + 1, 2):
+    sub_str += str(i) + ' '
+    if i % 5 == 0:
+        sub_str += '\n'
+
+print(sub_str)
