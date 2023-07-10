4dca1a87fdb2ed90b12417b6c19ffb795ae5ba09
evlev777 <levkovich.evgeniy.98@gmail.com>
2023-07-10 14:21:39 +0300

    change task6_4

--- Homework_4/lesson6_4.py
+++ Homework_4/lesson6_4.py
@@ -2,6 +2,19 @@
 # образом, чтобы остались только строки, использование дополнительного списка
 # незаконно
 
-print(list(filter(lambda x: isinstance(x, str), [1, 'ssd', 3.14, 'qweqwe', True, 'zxcxzcvz', ['a', 'b'], 'tgb'])))
+# print(list(filter(lambda x: isinstance(x, str), [1, 'ssd', 3.14, 'qweqwe', True, 'zxcxzcvz', ['a', 'b'], 'tgb'])))
+
+data = [1, 'ssd', 3.14, 'qweqwe', True, 'zxcxzcvz', ['a', 'b'], 'tgb']
+
+def filter_by_str(user_list):
+
+    for i in range(len(user_list) - 1, -1, -1):
+        if not isinstance(user_list[i], str):
+            del user_list[i]
+
+    return user_list
 
 
+res = filter_by_str(data)
+
+print(res)
